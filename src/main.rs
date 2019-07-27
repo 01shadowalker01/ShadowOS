df5fa47ccecdeff1a5c31b37cf0a3f8c5a4366c5
ShadoWalker <saeed.persian1998@gmail.com>
2019-07-27 23:57:06 +0430

    files moved to new repo

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
+
+use core::panic::PanicInfo;
+mod vga_buffer;
+
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    // this function is the entry point, since the linker looks for a function
+    // named `_start` by default
+    println!("Hello World !\n");
+    print!("ShadowOs!-+-@user >");
+
+    loop {}
+}
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
