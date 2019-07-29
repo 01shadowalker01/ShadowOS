b9b7fe1e33f217b7b54732f28da8ceadf6bab6f1
ShadoWalker <saeed.persian1998@gmail.com>
2019-07-29 07:24:53 +0430

    handling first exception: breakpoint exception finished

--- src/intrupt.rs
+++ src/intrupt.rs
@@ -1,5 +1,36 @@
-use x86_64::structures::idt::InterruptDescriptorTable;
+// The x86-interrupt calling convention leads to the following LLVM error
+// when compiled for a Windows target: "offset is not a multiple of 16". This
+// happens for example when running `cargo test` on Windows. To avoid this
+// problem we skip compilation of this module on Windows.
+#![cfg(not(windows))]
+
+use crate::println;
+use lazy_static::lazy_static;
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+
+lazy_static! {
+    static ref IDT: InterruptDescriptorTable = {
+        let mut idt = InterruptDescriptorTable::new();
+        idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt
+    };
+}
 
 pub fn init_idt() {
-    let mut idt = InterruptDescriptorTable::new();
+    IDT.load();
+}
+
+extern "x86-interrupt" fn breakpoint_handler(stack_frame: &mut InterruptStackFrame) {
+    println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
+}
+
+#[cfg(test)]
+use crate::{serial_print, serial_println};
+
+#[test_case]
+fn test_breakpoint_exception() {
+    serial_print!("test_breakpoint_exception...");
+    // invoke a breakpoint exception
+    x86_64::instructions::interrupts::int3();
+    serial_println!("[ok]");
 }
